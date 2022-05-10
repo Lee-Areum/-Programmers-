e6d30cda57160c694afd9b2eaf10ffaa0819a386
dldkfma9577 <arlee957790@gmail.com>
2022-05-10 18:42:03 +0900

    2022.05.10 오늘치 공부

--- Programmers2/다리를 지나는 트럭.cpp
+++ Programmers2/다리를 지나는 트럭.cpp
@@ -1,42 +1,25 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <iostream>
 
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-	int answer = 0;
-	int nowWeight = 0;
-	queue<int> bridge;
-	int index = 0;
-	int now = 0;
-	while(now<truck_weights.size()){
-		int n = truck_weights[now];
-		if (nowWeight + n <= weight) {
-			cout << "if" << endl;
-			bridge.push(n);
-			nowWeight += n;
-			now++;
-		}
-		else {
-			cout << "else" << endl;
-			for (int i = 0; i < bridge_length - 1; i++) {
-				bridge.push(0);
-			}
-			nowWeight -= truck_weights[index++];
-		}
-	}
-	for (int i = 0; i < bridge_length; i++) {
-		bridge.push(0);
-	}
-	while (!bridge.empty()) {
-		cout << bridge.front() << " ";
-		answer++;
-		bridge.pop();
-	}
-	return answer;
+    int answer = 0;
+    int i = 0,nowWeight = 0;
+    vector<pair<int, int>> v;
+    while (i < truck_weights.size() || !v.empty()) {
+        if (!v.empty() && v[0].first <= 0) {
+            nowWeight -= v[0].second;
+            v.erase(v.begin());
+        }
+        if (i < truck_weights.size() && nowWeight + truck_weights[i] <= weight) {
+            nowWeight += truck_weights[i];
+            v.push_back({ bridge_length,truck_weights[i++] });
+        }
+        for (int k = 0; k < v.size(); k++) {
+            v[k].first -= 1;
+        }
+        answer++;
+    }
+    return answer;
 }
-
-
-//https://programmers.co.kr/learn/courses/30/lessons/42583
